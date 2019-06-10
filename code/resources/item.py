e6870e573cb15b07dda413d2a3b5f6c8aedf5c3f
Madhangog <madhanmanoj2017@gmail.com>
2019-06-10 15:30:02 +0530

    separating models and resources

--- code/resources/item.py
+++ code/resources/item.py
@@ -1,6 +1,7 @@
 import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
+from models.items import ItemModel
 
 
 
@@ -19,7 +20,7 @@ class Item(Resource):
 		# 		return item
 		# item = next(iter(filter(lambda x: x['name'] == name , items)), None)
 		# return {'item' : item} , 200 if item else 404 #not found
-		item = self.find_by_name(name)
+		item = ItemModel.find_by_name(name)
 		if item:
 			return item
 		# connection = sqlite3.connect('data.db')
@@ -35,23 +36,11 @@ class Item(Resource):
 		return {'message': 'item not found'}, 404
 
 
-	@classmethod
-	def find_by_name(cls,name):
-		connection = sqlite3.connect('data.db')
-		cursor = connection.cursor()
-
-		query = "SELECT * FROM items WHERE name=?"
-		result = cursor.execute(query, (name,))
-		row = result.fetchone()
-		connection.close()
-
-		if row:
-			return {'item':{'name': row[0], 'price': row[1]}}
-
+	
 	def post(self, name):
 		# item = next(iter(filter(lambda x: x['name'] == name , items)), None)
 		# if item :
-		if self.find_by_name(name):
+		if ItemModel.find_by_name(name):
 			return {'message': "An Item with name '{}'already exists".format(name)}, 400 #bad requesr
 	    	# if next(iter(filter(lambda x: x['name'] == name , items)), None):
 		# data = request.get_json()
@@ -68,24 +57,11 @@ class Item(Resource):
 		# connection.commit()
 		# connection.close()
 		try:
-			self.insert(item)
+			ItemModel.insert(item)
 		except:
 			return {'message': "An error accured inserting the item"}, 500 #internal server error
 
 		return item , 201 #item create
-
-
-	@classmethod
-	def insert(cls,item):
-		connection = sqlite3.connect('data.db')
-		cursor = connection.cursor()
-
-		query = "INSERT INTO items VALUES ( ?, ?)"
-		cursor.execute(query , (item['name'], item['price']))
-
-		connection.commit()
-		connection.close()
-
 
 
 	def delete(self, name):
@@ -105,14 +81,14 @@ class Item(Resource):
 	def put(self,name):
 		data = Item.parser.parse_args()
 
-		item = self.find_by_name(name)
+		item = ItemModel.find_by_name(name)
 		updated_item = {'name': name, 'price': data['price']}
 
 		# data = request.get_json()
 		#item = next(iter(filter(lambda x: x['name'] == name , items)), None)
 		if item is None:
 			try:
-				self.insert(updated_item)
+				ItemModel.insert(updated_item)
 			except:
 				return {'message': "AN error occured inserting item"}, 500
 
@@ -120,23 +96,12 @@ class Item(Resource):
 			# items.append(item)
 		else:
 			try:
-				self.update(updated_item)
+				ItemModel.update(updated_item)
 			except:
 				return {"message": " An error occured inserting an item"}, 500
 			# item.update(data)
 		return updated_item
 
-
-	@classmethod
-	def update(cls, item):
-		connection = sqlite3.connect('data.db')
-		cursor = connection.cursor()
-
-		query = "UPDATE  items SET price=? WHERE name=?"
-		cursor.execute(query,(item['price'], item['name']))
-
-		connection.commit()
-		connection.close()
 
 
 class ItemList(Resource):
--- code/security.py
+++ code/security.py
@@ -1,6 +1,5 @@
-
 from werkzeug.security import safe_str_cmp
-from user import User
+from models.user import UserModel 
 
 # users = [ 
 # 	User(1, 'maddy', 'asdf')
@@ -30,10 +29,10 @@ from user import User
 
 def authenticate(username,password):
 	# user = username_mapping.get(username, None)
-	user = User.find_by_username(username)
+	user = UserModel.find_by_username(username)
 	if user and safe_str_cmp(user.password, password): #user.password == password
 		return user
 
 def identity(payload):
 	user_id = payload['identity']
-	return User.find_by_id(user_id)
+	return UserModel.find_by_id(user_id)
